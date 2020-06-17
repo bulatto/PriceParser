2949711e9a901cae3f5feb66bbd7c6366580bc2a
Bulat <nurg.bulat@mail.ru>
2020-06-17 18:17:17 +0300

    Обновление информации о настройках проекта

--- project/parsing/site_config/settings.py
+++ project/parsing/site_config/settings.py
@@ -1,15 +1,10 @@
 import os
 
-from django.core.exceptions import ImproperlyConfigured
-
 from config.settings.base import PROJECT_SETTINGS_DIR
 
 
 # Расположение конфигураций сайтов
-SITE_CONFIG_PATH = os.getenv('SITE_CONFIG_PATH')
-if not SITE_CONFIG_PATH or not os.path.exists(SITE_CONFIG_PATH):
-    # Дефолтное расположение файла конфиграций сайтов
-    SITE_CONFIG_PATH = os.path.join(PROJECT_SETTINGS_DIR, 'site_config.txt')
-    if not os.path.exists(SITE_CONFIG_PATH):
-        raise ImproperlyConfigured(
-            'Некорректно задан путь до конфигураций сайтов SITE_CONFIG_PATH!')
+SITE_CONFIG_PATH = os.path.join(PROJECT_SETTINGS_DIR, 'site_config.txt')
+if not os.path.exists(SITE_CONFIG_PATH):
+    # Если файла нет, создаётся пустой файл
+    open(SITE_CONFIG_PATH, 'w').close()
